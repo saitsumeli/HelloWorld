a4643a02451ee8be52e0b9bff6fd8fb717a4dc15
Sait Sumeli <Sait Sumeli@DESKTOP-EE5HQAU>
2021-11-30 00:58:15 +0300

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+
+
+        }
+        static void TekMiÇiftMi()
+        {
+            while (true)
+            {
+                Console.WriteLine("Bir sayı giriniz");
+                double s;
+                s = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bir sayı giriniz");
+                double d;
+                d = Convert.ToDouble(Console.ReadLine());
+                double x = 17, y = 18;
+                double a;
+                Console.WriteLine("Hello World!");
+                a = x / y;
+                double c = s + d;
+                if (c % 2 == 0)
+                {
+                    Console.WriteLine(c + "Çift sayıdır");
+                    break;
+                }
+                else Console.WriteLine(c + "Tek sayıdır");
+
+            }
+        }
+        
+    }
+}
